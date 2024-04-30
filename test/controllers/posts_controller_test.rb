6839b67503eb5fafda0bd8dbf8aa2f71ff352afe
arakikeita <araki1728@gmail.com>
2024-04-30 23:50:04 +0900

    test5

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
  # test "the truth" do
   #CwsSecurityManager cwsSecurityManager = new CwsSecurityManager();
 
+  #   assert true
+  #CwsSecurityManager cwsSecurityManager = new CwsSecurityManager();
+
 class PostsControllerTest < ActionDispatch::IntegrationTest
  
   #   assert true
@@ -32,12 +35,18 @@ end
 
 class Test1  # test "the truth" do
  
-  #   assert true
+
   # end
 end
 
 class Test4  # test "the truth" do
  
-  #   assert true
+
+  # end
+end
+
+class Test5  # test "the truth" do
+ 
+  
   # end
 end
